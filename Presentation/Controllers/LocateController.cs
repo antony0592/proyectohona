cb5ecf7d260fdea634483adb8c09a4f8fb21f245
daniel5994488 <31112378+daniel5994488@users.noreply.github.com>
2020-04-17 10:37:35 -0600

    Cambios en la obtencion de datos para las vistas
    Se cambiaron las paginas facility y locate

--- Presentation/Controllers/LocateController.cs
+++ Presentation/Controllers/LocateController.cs
@@ -4,16 +4,33 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Presentation.Models;
+using Domain;
 
 
 namespace Presentation.Controllers
 {
     public class LocateController : Controller
     {
+        private readonly IConfiguration _configuration;
+        string connectionString = "";
+
+        public LocateController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
         // GET: Locate us
         public ActionResult Home()
         {
+            RepositoryHotel hotelModel = new RepositoryHotel(connectionString);
+            Hotel hotels = hotelModel.GetAllHotel();
+            HotelModel hotel = new HotelModel();
+            hotel.address = hotels.address;
+
+            ViewBag.hotel = hotel;
             return View();
         }
     }
