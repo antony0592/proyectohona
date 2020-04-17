f909f9b502d7fcfab790699084b50e1437a2fddb
Anthony S. A <asalazar0592@gmail.com>
2020-04-17 14:57:27 -0600

    Cambios en CSS
    Creación de nuevas clases CSS para títulos, navbars, hovers, estilos de letras, y fondos de div.

--- Presentation/wwwroot/js/site.js
+++ Presentation/wwwroot/js/site.js
@@ -6,3 +6,20 @@
 $(document).ready(function () {
     $('#summernote').summernote();
 });
+
+/*Efecto Titulo*/
+$(function () {
+    var $jittery = $('.titulo'),
+        aText = $jittery.text().split(''),
+        letters = '';
+
+    for (var i = 0; i < aText.length; i++) {
+        letters += '<span>' + aText[i] + '</span>';
+    }
+
+    $jittery.empty().append(letters);
+
+    $.each($('span', $jittery), function (i) {
+        $(this).css('animation-delay', '-' + i + '70ms');
+    });
+});
