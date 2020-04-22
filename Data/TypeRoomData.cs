2fa5e5be5b10e3e3422b80bfad39b8eeb42b13ff
MabelHeller <arguedas3023@gmail.com>
2020-04-22 01:06:53 -0600

    Obtención de datos para tipo de habitación
    Co-Authored-By: daniel5994488 <daniel5994488@users.noreply.github.com>

---
+++ Data/TypeRoomData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Domain;
+
+namespace Data
+{
+    public class TypeRoomData
+    {
+        private String connString;
+
+        public TypeRoomData(String connString)
+        {
+            this.connString = connString;
+        }//Fin del constructor.
+
+        
+       public List<TypeRoom> GetAllTypeRoom()
+        {
+
+            List<TypeRoom> typeRoom = new List<TypeRoom>();
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SelectTypeRoom", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                //this reads all the rows coming from DB
+                while (sqlDataReader.Read())
+                {
+                    typeRoom.Add(new TypeRoom
+                    {
+                        id = Convert.ToInt32(sqlDataReader["id"]),
+                        quantityperson = Convert.ToInt32(sqlDataReader["quantityperson"]),
+                        quantitybed = Convert.ToInt32(sqlDataReader["quantitybed"]),
+                        description = sqlDataReader["description"].ToString(),
+                        amount = Convert.ToInt32(sqlDataReader["amount"]),
+                        urlimage = sqlDataReader["urlimage"].ToString(),
+                        descriptiontyperoom = sqlDataReader["descriptiontyperoom"].ToString()
+
+                    });
+                }
+                connection.Close();
+            }
+            return typeRoom;
+        }
+    }
+}
