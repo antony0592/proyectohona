41617e5d9f7baa3da66975629810814be8fbb9c9
MabelHeller <arguedas3023@gmail.com>
2020-05-27 22:50:03 -0600

    Lista de Reservas

---
+++ Admin/Data/ReservationData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Domain;
+
+namespace Data
+{
+    public class ReservationData
+    {
+        private String connString;
+
+        public ReservationData(String connString)
+        {
+            this.connString = connString;
+        }//Fin del constructor.
+
+
+        public List<Reservation> GetAllReservation()
+        {
+            List<Reservation> reservation = new List<Reservation>();
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SelectReservation", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                //this reads all the rows coming from DB
+                while (sqlDataReader.Read())
+                {
+                    reservation.Add(new Reservation
+                    {
+                        id = Convert.ToInt32(sqlDataReader["id"]),
+                        idroom = Convert.ToInt32(sqlDataReader["idroom"]),
+                        idclient = Convert.ToInt32(sqlDataReader["idclient"]),
+                        amount = Convert.ToInt32(sqlDataReader["amount"]),
+                        arrivaldate = Convert.ToDateTime(sqlDataReader["arrivaldate"]),
+                        departuredate = Convert.ToDateTime(sqlDataReader["departuredate"])
+                    });
+                }
+                connection.Close();
+            }
+            return reservation;
+        }
+
+    }
+}
--- Admin/Presentation/Controllers/AdministratorReservationListController.cs
+++ Admin/Presentation/Controllers/AdministratorReservationListController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Presentation.Models;
+using Domain;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,11 +14,37 @@ namespace Presentation.Controllers.Administrator
     [Authorize(Roles = "Admin")]
     public class AdministratorReservationListController : Controller
     {
+        public static string counter;
+        private readonly IConfiguration _configuration;
+        string connectionString = "";
+
+        public AdministratorReservationListController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
         // GET: /<controller>/
         [Authorize]
         public IActionResult ReservationList()
         {
-            return View();
-        }
+            ReservationModel repositoryReservation = new ReservationModel(connectionString);
+            IList<Reservation> reservation = repositoryReservation.GetAllReservation();
+            List<ReservationModel> reservationModel = new List<ReservationModel>();
+
+
+            for (int i = 0; i < reservation.Count; i++)
+            {
+                ReservationModel reservationNew = new ReservationModel();
+                reservationNew.id = reservation[i].id;
+                reservationNew.idroom = reservation[i].idroom;
+                reservationNew.idclient = reservation[i].idclient;
+                reservationNew.amount = reservation[i].amount;
+                reservationNew.arrivaldate = reservation[i].arrivaldate;
+                reservationNew.departuredate = reservation[i].departuredate;
+                reservationModel.Add(reservationNew);
+            }
+            return View(reservationModel);
+        }//
     }
 }
