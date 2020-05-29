35ab7444b156de1a5dea39faebcb4844491e3ec5
nasg2013 <62192753+nasg2013@users.noreply.github.com>
2020-05-29 17:27:09 -0600

    Update AdministratorAvailabilityData.cs

--- Admin/Data/AdministratorAvailabilityData.cs
+++ Admin/Data/AdministratorAvailabilityData.cs
@@ -1,7 +1,10 @@
-﻿using Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Text;
+using Domain;
 
 
 namespace Data
@@ -16,9 +19,34 @@ namespace Data
             this.connString = connString;
         }//Fin del constructor.
 
-        //public List<SearchAvailabilityResult> SearchAvailability() 
-        //{
+        public List<SearchAvailabilityResult> GetsearchroomAvailability(string date1, string date2, int typeroomparameter)
+        {
+            List<SearchAvailabilityResult> room = new List<SearchAvailabilityResult>();
 
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SearchRoom", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add("@arrivaldate", SqlDbType.Date).Value = date1;
+                command.Parameters.Add("@departuredate", SqlDbType.Date).Value = date2;
+                command.Parameters.Add("@typeroom", SqlDbType.Int).Value = typeroomparameter;
+
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                //this reads all the rows coming from DB
+                while (sqlDataReader.Read())
+                {
+                    room.Add(new SearchAvailabilityResult
+                    {
+                        typeroom = sqlDataReader["typeroom"].ToString(),
+                        number = Convert.ToInt32(sqlDataReader["number"]),
+                        amount = Convert.ToInt32(sqlDataReader["amount"])
+                    });
+                }
+                connection.Close();
+            }
+            return room;
         }
+
     }
 }
